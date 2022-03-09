b9afc906189caed308b2f360d11df8c48109ba5b
okta <o1093020@pu.edu.tw>
2022-03-09 10:41:48 +0800

    第一次 commit

---
+++ app/src/main/java/tw/edu/pu/o10930200/hello22/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.o10930200.hello22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
